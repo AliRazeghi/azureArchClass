bb8b85a8a7f13bdc95935eadecb68d5296c2473a
Ali Razeghi <aliraz@gmail.com>
2020-04-27 11:56:31 -0700

    monday EOD commit. Clean up needed.

--- buildupTearDown.ps1
+++ buildupTearDown.ps1
@@ -37,14 +37,21 @@ $vnetName = 'PacktLBVnet'
 $vnetAddressPrefix = '172.16.0.0/16'
 $subnetName = 'PacktLBBackendSubnet'
 $subnetAddressPrefix = '172.16.0.0/24'
-## Load Balancers
+###### Load Balancers
 $LBName = $vnetRGName
 $LBLocation = $vnetLoc
 $LBRG  = $vnetRGName
 $LBVN = $vnetName
 $LBSubnet = $subnetName
 $PrivIPSpace = '172.16.0.1'
-
+$LBfrontendIP = '172.16.0.7'
+##Probes for LBs
+$LBProbeName = 'PacktHealthProbe'
+$LBProbePort = '80'
+$LBProbePath =  '/.'
+$LBProbebProtocol = 'HTTP'
+$LBProbeInterval = '15'        ##Number of seconds between attempts
+$LBProbeCount = '2'  ##How many times must it fail to be unhealthy?
 
 ##################################### VM Variables #########################################
 $VMRG = $vnetRGName ##Company policy dictates all machines be in the RG as their NIC
@@ -70,7 +77,7 @@ write-verbose "doesvnetRGExist Variable Value: $($doesvnetRGExist)"
 if ($doesvnetRGExist) {write-host "RG: $($doesvnetRGExist.ResourceGroupName) exists. Not creating RG $($DoesVNetRGExist.ResourceGroupName). Reusing Old one." -ForegroundColor Green} 
 else {
 write-host "creataing RG: $($vnetRGName) in $($vnetLoc)" -ForegroundColor Green
-New-AzResourceGroup -Name $($vnetRGName) -Location $($vnetLoc) | 
+New-AzResourceGroup -Name $($vnetRGName) -Location $($vnetLoc) | out-null
 }
 
 
@@ -156,7 +163,7 @@ $doesVMExist = get-azvm -Name $($VM1Name)
 if ($doesVMExist) {write-host "VM Found named: $($VM1Name). Continuing" -foreground Green}
 else
 {
-write-host "vm does not exist. Creating now." -ForegroundColor Yellow
+write-host "vm: $($VM1Name) does not exist. Creating now." -ForegroundColor Yellow
 #$Cred = get-credential          #UNCOMMENT!
 New-AzVm `
         -ResourceGroupName "$($VMRG)" `
@@ -177,7 +184,7 @@ $doesVMExist = get-azvm -Name $($VM2Name)
 if ($doesVMExist) {write-host "VM Found named: $($VM2Name). Continuing" -foreground Green}
 else
 {
-write-host "vm does not exist. Creating now." -ForegroundColor Yellow
+write-host "vm: $($VM2Name) does not exist. Creating now." -ForegroundColor Yellow
 New-AzVm `
         -ResourceGroupName "$($VMRG)" `
         -Name "$($VM2Name)" `
@@ -196,7 +203,7 @@ $doesVMExist = get-azvm -Name $($VM3Name)
 if ($doesVMExist) {write-host "VM Found named: $($VM3Name). Continuing" -foreground Green}
 else
 {
-write-host "vm does not exist. Creating now." -ForegroundColor Yellow
+write-host "vm: $($VM3Name) does not exist. Creating now." -ForegroundColor Yellow
 New-AzVm `
         -ResourceGroupName "$($VMRG)" `
         -Name "$($VM3Name)" `
@@ -212,14 +219,53 @@ New-AzVm `
 
 
 
-
-
-
 ### Load Balancers for VMs
-
+### Load Balancers need their configurations done first, then tied to the LB, similar to networking in a VM.
 <#
+xx Load Balancer itself
+Backend Pool
+xx Health Checks
+xx LB Rules
+
+add ips to the backend pools or tie to a nic of our 2 machines.
 #>
-$doesLBExist = Get-AzLoadBalancer -Name $($LBName) -ResourceGroupName $($LBRG) | select-object {$_.Name} #| out-null   ##Will have to look into why this requires a select-object. 
+
+if ($doesLBExist){write-host "LB $($LBName) exists. Removing." -ForegroundColor Green; Remove-AzLoadBalancer -Name PacktLoadBalancer -ResourceGroupName PacktLoadBalancer -force}
+
+
+##Configure backend pool
+$LBBackendPool = New-AzLoadBalancerBackendAddressPoolConfig -Name "PacktLBBackendPool" 
+
+##Load Balancer Probe
+$LoadBalancerProbe = New-AzLoadBalancerProbeConfig -Name $($LBProbeName) -Protocol $($LBProbebProtocol) -Port $LBProbePort -RequestPath $($LBProbePath) -IntervalInSeconds $($LBProbeInterval)  -ProbeCount $($LBProbeCount) 
+
+
+##Load Balancer Rules
+$LBRules = New-AzLoadBalancerRuleConfig -Name PacktLoadBalancerRule -BackendAddressPool PacktLBBackendPool -BackendPort 80 -FrontendIpConfiguration LoadBalancerFrontEnd -FrontendPort 80 -IdleTimeoutInMinutes 4 -Probe PacktHealthProbbe -Protocol TCP
+
+
+
+
+
+## Actual work.
+$doesLBExist = Get-AzLoadBalancer -Name $($LBName) -ResourceGroupName $($LBRG) -ErrorAction SilentlyContinue  #| select-object {$_.Name} | out-null   ##Will have to look into why this requires a select-object. 
 if ($doesLBExist){write-host "LB $($LBName) exists." -ForegroundColor Green}
-else {write-host "creating LB $($LBName)" -ForegroundColor Yellow; New-AzLoadBalancer -ResourceGroupName $($LBRG) -Name $($LBName) -Location $($LBLocation) -Sku "Basic" | out-null}
+else {write-host "creating LB $($LBName)" -ForegroundColor Yellow; New-AzLoadBalancer -ResourceGroupName $($LBRG) -Name $($LBName) -Location $($LBLocation) -Sku "Basic" -BackendAddressPool $LBBackendPool -Probe $LoadBalancerProbe -LoadBalancingRule $LBRules} #  | out-null}
+#Remove-AzLoadBalancer -ResourceGroupName $LBRG -Name $LBName -force -verbose
+
+
+    $Subnet = Get-AzVirtualNetwork -Name "PacktLBVnet" -ResourceGroupName "PacktLoadBalancer" | Get-AzVirtualNetworkSubnetConfig -Name "PacktLBBackendSubnet"
+    $slb = Get-AzLoadBalancer -Name "PacktLoadBalancer" -ResourceGroupName "PacktLoadBalancer"
+    $slb | Add-AzLoadBalancerFrontendIpConfig -Name "NewFrontend" -Subnet $Subnet -PrivateIpAddress '172.16.0.7'
+    $slb | set-azloadbalancer  ##Wont set otherwise.
+
+
+
+
+#Set-AzLoadBalancerFrontendIpConfig -LoadBalancer 'PacktLoadBalancer' -Name 'PacktLoadBalancerConfig' -Subnet PacktLBBackendSubnet -PrivateIpAddress 172.16.0.7
+#$frontend = New-AzLoadBalancerFrontendIpConfig -Name "MyFrontEnd" -PublicIpAddress $publicip
+    
+
+
+
 

322c4c4d030ec7aaf8eebcb2389f8dc98d52be06
Ali Razeghi <aliraz@gmail.com>
2020-05-04 11:03:59 -0700

    read me

---
+++ BuildLoadBalancerWithSettingsFirst.ps1
@@ -0,0 +1,54 @@
+﻿
+### To build a brand new load balancer and pass all the values ahead of tie.
+
+# Variables for common values
+$rgName='TestRG'
+$location='eastus'
+
+# Create user object
+#$cred = Get-Credential -Message 'Enter a username and password for the virtual machine.'
+
+# Create a resource group.
+New-AzResourceGroup -Name $rgName -Location $location
+
+# Create a virtual network.
+$subnet = New-AzVirtualNetworkSubnetConfig -Name 'MySubnet' -AddressPrefix 192.168.1.0/24
+
+$vnet = New-AzVirtualNetwork -ResourceGroupName $rgName -Name 'MyVnet' `
+  -AddressPrefix 192.168.0.0/16 -Location $location -Subnet $subnet
+
+# Create a public IP address.
+$publicIp = New-AzPublicIpAddress -ResourceGroupName $rgName -Name 'myPublicIP' `
+  -Location $location -AllocationMethod Dynamic
+
+# Create a front-end IP configuration for the website.
+$feip = New-AzLoadBalancerFrontendIpConfig -Name 'myFrontEndPool' -PublicIpAddress $publicIp
+
+# Create the back-end address pool.
+$bepool = New-AzLoadBalancerBackendAddressPoolConfig -Name 'myBackEndPool'
+
+# Creates a load balancer probe on port 80.
+$probe = New-AzLoadBalancerProbeConfig -Name 'myHealthProbe' -Protocol Http -Port 80 `
+  -RequestPath / -IntervalInSeconds 360 -ProbeCount 5
+
+# Creates a load balancer rule for port 80.
+$rule = New-AzLoadBalancerRuleConfig -Name 'myLoadBalancerRuleWeb' -Protocol Tcp `
+  -Probe $probe -FrontendPort 80 -BackendPort 80 `
+  -FrontendIpConfiguration $feip -BackendAddressPool $bePool
+
+# Create three NAT rules for port 3389.
+$natrule1 = New-AzLoadBalancerInboundNatRuleConfig -Name 'myLoadBalancerRDP1' -FrontendIpConfiguration $feip `
+  -Protocol tcp -FrontendPort 4221 -BackendPort 3389
+
+$natrule2 = New-AzLoadBalancerInboundNatRuleConfig -Name 'myLoadBalancerRDP2' -FrontendIpConfiguration $feip `
+  -Protocol tcp -FrontendPort 4222 -BackendPort 3389
+
+$natrule3 = New-AzLoadBalancerInboundNatRuleConfig -Name 'myLoadBalancerRDP3' -FrontendIpConfiguration $feip `
+  -Protocol tcp -FrontendPort 4223 -BackendPort 3389
+
+# Create a load balancer.
+$lb = New-AzLoadBalancer -ResourceGroupName $rgName -Name 'MyLoadBalancer' -Location $location `
+  -FrontendIpConfiguration $feip -BackendAddressPool $bepool `
+  -Probe $probe -LoadBalancingRule $rule -InboundNatRule $natrule1,$natrule2,$natrule3
+
+
